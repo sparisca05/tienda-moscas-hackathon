488f91b3e1943eb07f27c82826b980359039182a
Simon <simon.parisca@eia.edu.co>
2024-11-10 15:43:49 -0500

    archivo .env

--- back/src/main/java/com/example/tiendamoscas/TiendamoscasApplication.java
+++ back/src/main/java/com/example/tiendamoscas/TiendamoscasApplication.java
@@ -2,11 +2,18 @@ package com.example.tiendamoscas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TiendamoscasApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("FRONT_URL", dotenv.get("FRONT_URL"));
+
 		SpringApplication.run(TiendamoscasApplication.class, args);
 	}
 
